1973d9707c005eb7900a62df17616ca5022307a4
calleighwinberg <calleighwinberg@gmail.com>
2023-12-04 15:05:51 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import sendgrid
+import os
+
+import ssl
+ssl._create_default_https_context = ssl._create_stdlib_context
+
+from sendgrid.helpers.mail import Mail, Email, To, Content
+
+sg = sendgrid.SendGridAPIClient(api_key='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')
+from_email = Email("calleigh@seas.upenn.edu")  # Change to your verified sender
+to_email = To("calleighwinberg@gmail.com")  # Change to your recipient
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, to_email, subject, content)
+
+# Get a JSON-ready representation of the Mail object
+mail_json = mail.get()
+
+# Send an HTTP POST request to /mail/send
+response = sg.client.mail.send.post(request_body=mail_json)
+print(response.status_code)
+print(response.headers)
