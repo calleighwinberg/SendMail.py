a4e612525f98d31561816a71aef7aafbb31a70ff
calleighwinberg <calleighwinberg@gmail.com>
2023-12-04 17:38:01 -0800

    updated the mars_photo functionality. I would like to work on my params for the sendEmail function. Also not sure what html_content does. Having fun!

--- app.py
+++ app.py
@@ -1,22 +1,49 @@
+import random
+import requests
 import sendgrid
+from sendgrid.helpers.mail import Mail, Email, To, Content
 import os
-
 import ssl
 ssl._create_default_https_context = ssl._create_stdlib_context
 
-from sendgrid.helpers.mail import Mail, Email, To, Content
-
 sg = sendgrid.SendGridAPIClient(api_key='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')
-from_email = Email("calleigh@seas.upenn.edu")  # Change to your verified sender
-to_email = To("calleighwinberg@gmail.com")  # Change to your recipient
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "and easy to do anywhere, even with Python")
-mail = Mail(from_email, to_email, subject, content)
 
-# Get a JSON-ready representation of the Mail object
-mail_json = mail.get()
+apiNASA = 'dXIgIS03gugAFnz3mfdOUlLVkwS8iTRxqBx23xht'
+urlNASA = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
 
-# Send an HTTP POST request to /mail/send
-response = sg.client.mail.send.post(request_body=mail_json)
-print(response.status_code)
-print(response.headers)
+def getMarsPhoto(sol):
+    params = {'sol': sol, 'api_key': apiNASA}
+    response = requests.get(urlNASA, params)
+    if (response.status_code != 200):
+        print("Error")
+        return []
+
+    data = response.json()
+    photos = data['photos']
+
+    photoHTML = random.choice(photos)['img_src']
+
+    return photoHTML
+
+
+'''
+'''
+def sendEmail(fromEmail, toEmail, subject, img_url):
+    mail = Mail(fromEmail, toEmail, subject, html_content='<strong>Check out this Mars pic</strong><br>'f'<img src="{img_url}"></img>')
+
+    # Get a JSON-ready representation of the Mail object
+    mail_json = mail.get()
+
+    # Send an HTTP POST request to /mail/send
+    response = sg.client.mail.send.post(request_body=mail_json)
+    print(response.status_code)
+    print(response.headers)
+
+
+fromEmail = "calleigh@seas.upenn.edu"  # Change to your verified sender
+toEmail = "calleighwinberg@gmail.com"  # Change to your recipient
+subject = "Daily Mars Photo"
+img_url = getMarsPhoto('1000')
+#content = '<strong>Check out this Mars pic</strong><br>'f'<img src="{img_url}"></img>')
+
+sendEmail(fromEmail, toEmail, subject, img_url)
