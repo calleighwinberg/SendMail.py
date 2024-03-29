5f1913723d4cb86fcde094f38f6be85f2fbe5b82
calleighwinberg <calleighwinberg@gmail.com>
2024-03-29 15:03:14 -0700

    reconfiguring wsgi file and debugging for deployment

--- flask_interface.py
+++ flask_interface.py
@@ -29,3 +29,6 @@ def email_response():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+'''if __name__ == '__main__':
+    app.run(debug=True)'''
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from flask_interface import app
+import re
+from mars_email import get_mars_photo, send_email
 
 if __name__ == '__main__':
     app.run()
