f2ec216772f4ab1fd608221824c33eb37ed02a4b
calleighwinberg <calleighwinberg@gmail.com>
2024-03-29 16:45:26 -0700

    changing port

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
-'''import os
+import os
 
 from flask_interface import app
-import re
-from mars_email import get_mars_photo, send_email
 
 if __name__ == '__main__':
-    app.run()'''
+    app.run()
